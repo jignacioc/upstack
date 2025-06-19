ab3cf846421144f74605a234313928f7dac1d682
Joaquin Ignacio Clark <93284996+jignacioc@users.noreply.github.com>
2025-06-19 09:27:16 -0400

    Fix: Backups

--- db-backup/backup.sh
+++ db-backup/backup.sh
@@ -4,9 +4,17 @@ set -e
 DATE=$(date +%F_%H-%M)
 BACKUP_FILE="/backups/backup_${DATE}.dump"
 
-echo "📦 Iniciando backup a ${BACKUP_FILE}..."
+echo "[$(date)] Iniciando backup a ${BACKUP_FILE}..."
 
-PGPASSWORD=$POSTGRES_PASSWORD pg_dump -h $POSTGRES_HOST -U $POSTGRES_USER -F c -d $POSTGRES_DB > "$BACKUP_FILE"
+# Validar variables mínimas
+if [[ -z "$POSTGRES_PASSWORD" || -z "$POSTGRES_HOST" || -z "$POSTGRES_USER" || -z "$POSTGRES_DB" ]]; then
+    echo "❌ Variables POSTGRES_* no definidas correctamente"
+    exit 1
+fi
+
+export PGPASSWORD="$POSTGRES_PASSWORD"
+
+pg_dump -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -F c -d "$POSTGRES_DB" -f "$BACKUP_FILE"
 
 echo "✅ Backup realizado con éxito."
 
